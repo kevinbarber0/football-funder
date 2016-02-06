0c0e89e7c6c988d2f3b7bb12aa185dde51e6ebb3
root <kevinbarber0@outlook.com>
2016-02-06 09:17:57 -0700

    index.ts updated

--- src/Pages/Approval/Components/Components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AccountDetail';
-export * from './LisitngAction';
-export * from './NumberTextButton';
-export * from './ListingTableLabel';
-export * from './WithdrawalAction';
-export * from './WithdrawalTableLabel';
