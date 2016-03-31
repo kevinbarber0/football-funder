9c71a35990bcc651f86d5ec7922a8be222ae51d8
root <kevinbarber0@outlook.com>
2016-03-30 18:55:36 -0600

    SignUpButton.tsx updated

--- src/Pages/LandingPage/Component/LandingSignUpForm/SignUpButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const SignUpButton: React.FC = () => {
-  return (
-    <button className="bg-green-10 w-full  py-3 font-semibold rounded-10 text-center ">
-      Sign Up
-    </button>
-  );
-};
