ba5b1d280260476b28880027ab800558c1b12f5f
root <kevinbarber0@outlook.com>
2016-04-01 15:39:06 -0600

    messagesType.ts added

---
+++ src/types/messagesType.ts
@@ -0,0 +1,10 @@
+export enum QueryKey {
+  accountVerify = 'account-verify',
+  CrowdingFundingList = 'get-crowdingfunding-list',
+  AccountFundraiserList = 'get-accountfundraiser-list',
+  AdsList = 'get-advertising-list',
+  AccountAdvertisingList = 'get-accountadvertising-list',
+  SponsorshipList = 'get-sponsorship-list',
+  AccountSponsorshipList = 'get-accountsponsorship-list',
+  AllListData = 'get-all-list'
+}
