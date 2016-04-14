adc5ed9bc7fe0964a881013d89ded7f306b956ed
root <kevinbarber0@outlook.com>
2016-04-14 11:48:29 -0600

    myDropdownType.ts added

---
+++ src/types/ComponentsTypes/myDropdownType.ts
@@ -0,0 +1,11 @@
+export interface MyDropdownType {
+    title?: string,
+    href?: string,
+    icon?: any,
+    children: MyDropdownItemType[]
+};
+export interface MyDropdownItemType {
+    title?: string,
+    href?: string,
+    icon?: any
+};
