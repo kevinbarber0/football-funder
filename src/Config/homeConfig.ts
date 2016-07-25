68399d61d7fd421e70c47c555f05816c4836150c
root <kevinbarber0@outlook.com>
2016-07-25 15:06:20 -0600

    homeConfig.ts added

---
+++ src/Config/homeConfig.ts
@@ -0,0 +1,98 @@
+import { CardType } from "types";
+
+import FirstCarouselCard from "Assets/images/home/carousel-first-img.png";
+import SecondCarouselCard from 'Assets/images/home/carousel-second-image.png'
+
+export const CarouselCardData: CardType[] = [
+    {
+      broadcastingType: 'Live',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+    fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:FirstCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '100% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:SecondCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:SecondCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:FirstCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:SecondCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:FirstCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:FirstCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:SecondCarouselCard
+    },
+    {
+      broadcastingType: 'Funded',
+      title: 'Running a crowdfunding campaign for my football club',
+      description: 'The subheader for running a crowdfunding campaign for my football club goes here and there’s three lines of copy available to describe it.',
+      fund: '50% of goal',
+      progress:true,
+      curFund: 50000,
+      oriFund: 100000,
+      image:FirstCarouselCard
+    },
+
+];
