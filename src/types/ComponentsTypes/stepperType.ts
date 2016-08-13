bbaa250c204d82e80e2f04c901e23091d33f1128
root <kevinbarber0@outlook.com>
2016-08-13 15:58:51 -0600

    stepperType.ts added

---
+++ src/types/ComponentsTypes/stepperType.ts
@@ -0,0 +1,14 @@
+export interface StepperActionPropsType {
+  handlePrevPage: () => void;
+  handleNextPage: () => void;
+}
+export interface FirstStepperActionPropsType {
+  handleNextPage: () => void;
+}
+
+export interface LivePagePropsType { 
+  handlePrevPage: () => void;
+  handleNextPage: () => void;
+  pageTitle: string;
+  introTitle: string;
+}
